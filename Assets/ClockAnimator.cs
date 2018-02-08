a51c6b4e8e7a6f1a2787810ff5e7da2e4f0c329c
田园式编码 <30883923+Jade-Jade-Jade@users.noreply.github.com>
2018-02-07 18:42:17 -0800

    Initial commit

---
+++ Assets/ClockAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class ClockAnimator : MonoBehaviour {
+
+    public Transform hours, minutes, seconds;
+
+    private const float
+        HOURSTODEGREES = 360f / 12f,
+        MINTUESTODEGREES = 360f / 60f,
+        SECONDSTODEGREES = 360f / 60f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DateTime time = DateTime.Now;
+        hours.localRotation =
+            Quaternion.Euler(0f, 0f, time.Hour * -HOURSTODEGREES);
+        minutes.localRotation =
+            Quaternion.Euler(0f, 0f, time.Minute * -MINTUESTODEGREES);
+        seconds.localRotation =
+            Quaternion.Euler(0f, 0f, time.Second * -SECONDSTODEGREES);
+    }
+}
